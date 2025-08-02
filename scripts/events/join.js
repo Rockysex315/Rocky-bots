d06d72ea4f30a6a5200be69ef6206fabe5604024
Rockysex315 <brokenheart0090y8t@gmail.com>
2025-08-02 14:58:18 +0530

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -1,39 +1,3 @@
-module.exports.config = {
-  name: "join",
-  eventType: ['log:subscribe'],
-  version: "1.0.0",
-  credits: "Mirai-Team", // FIXED BY YAN Nayan
-  description: "GROUP UPDATE NOTIFICATION"
-};
-
-const fs = require('fs-extra');
-const { loadImage, createCanvas, registerFont } = require("canvas");
-const request = require('request');
-//const { join } = require('path');
-const axios = require('axios');
-const jimp = require("jimp")
-const fontlink = 'https://drive.google.com/u/0/uc?id=10XFWm9F6u2RKnuVIfwoEdlav2HhkAUIB&export=download'
-let PRFX = `${global.config.PREFIX}`;
-
-module.exports.circle = async (image) => {
-  image = await jimp.read(image);
-  image.circle();
-  return await image.getBufferAsync("image/png");
-}
-
-let suffix;
-
-module.exports.run = async function({ api, event, Users }) {
-  var fullYear = global.client.getTime("fullYear");
-  var getHours = await global.client.getTime("hours");
-  var session = `${getHours < 3 ? "midnight" : getHours < 8 ? "Early morning" : getHours < 12 ? "noon" : getHours < 17 ? "afternoon" : getHours < 23 ? "evening" : "midnight"}`
-  const moment = require("moment-timezone");
-  var thu = moment.tz('Asia/dhaka').format('dddd');
-  if (thu == 'Sunday') thu = 'Sunday'
-  if (thu == 'Monday') thu = 'Monday'
-  if (thu == 'Tuesday') thu = 'Tuesday'
-  if (thu == 'Wednesday') thu = 'Wednesday'
-  if (thu == "Thursday") thu = 'Thursday'
   if (thu == 'Friday') thu = 'Friday'
   if (thu == 'Saturday') thu = 'Saturday'
   const time = moment.tz("Asia/dhaka").format("HH:mm:ss - DD/MM/YYYY");
